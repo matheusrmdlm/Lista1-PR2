99c1f3ae964306f4ef3a0c486fe1b80c0b37ae94
CJ3016943 <CJ3016943@LAB01-PC39>
2023-03-08 10:55:38 -0300

    Adicionar arquivos de projeto.

---
+++ Lista1 PR2/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1_PR2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //EXEC 1
+            //int metro,cm,mm;
+            //console.writeline("digite o número de metros");
+            //metro=int.parse(console.readline());
+            //cm=metro*100;
+            //mm=metro* 1000;
+            //console.writeline("a quantidade em cm é "+cm);
+            //console.writeline("a quantidade em mm é "+mm);
+            //console.readkey();
+
+            //EXEC 2
+            // float fahr,resul;
+            // int   fahr1,resul1;
+            // Console.WriteLine("digite o número de Fahrenheit em float :");
+            // fahr=float.Parse(Console.ReadLine());
+            // Console.WriteLine("digite o número de Fahrenheit em int :");
+            // fahr1=int.Parse(Console.ReadLine());
+            // resul=(fahr-32f) * (5.0f/9.0f);
+            // resul1=(fahr1-32) * (5/9);
+            // Console.WriteLine("a quantidade em celsius em float é : "+resul);
+            // Console.WriteLine("a quantidade em celsius em int é : "+resul1);
+            // Console.ReadKey();
+
+            //EXEC 3
+            //    Console.Write("fale se peso: ");
+            //    double peso = double.Parse(Console.ReadLine());
+
+            //    Console.Write("fale a sua altura ");
+            //    double altura = double.Parse(Console.ReadLine());
+
+            //    double imc = peso / (altura * altura);
+
+            //    Console.WriteLine("Seu IMC é: {0:F2}", imc);
+
+            //EXEC 4
+            //Console.Write("Fale o 1° número: ");
+            //double num1 = double.Parse(Console.ReadLine());
+            //Console.Write("Fale o 2° número: ");
+            //double num2 = double.Parse(Console.ReadLine());
+            //Console.Write("Fale o 3° número: ");
+            //double num3 = double.Parse(Console.ReadLine());
+            //Console.Write("Fale a massa do 1° número: ");
+            //double peso1 = double.Parse(Console.ReadLine());
+            //Console.Write("Fale a massa do 2° número: ");
+            //double peso2 = double.Parse(Console.ReadLine());
+
+            //Console.Write("Fale a massa do 3° número: ");
+            //double peso3 = double.Parse(Console.ReadLine());
+
+            //double mediaPonderada = (num1 * peso1 + num2 * peso2 + num3 * peso3) / (peso1 + peso2 + peso3);
+            //Console.WriteLine("A média ponderada é: {0}", mediaPonderada);
+
+            //EXEC 5
+            //double massa, velocidade, energiaCinetica;
+
+            //Console.WriteLine("Digite a massa do objeto em kg:");
+            //massa = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Digite a velocidade do objeto em m/s:");
+            //velocidade = Convert.ToDouble(Console.ReadLine());
+
+            //energiaCinetica = 0.5 * massa * Math.Pow(velocidade, 2);
+
+            //Console.WriteLine("A energia cinética do objeto é de: " + energiaCinetica + " J");
+            //Console.ReadKey();
+
+            //EXEC 6
+            //double SalarioAtual, SalarioFuturo;
+            //Console.Write("Me informe qual é o salário do funcionário: ");
+            //SalarioAtual = double.Parse(Console.ReadLine());
+            //SalarioFuturo = SalarioAtual * 1.25;
+
+            //Console.WriteLine("O novo salário do funcionário é: " + SalarioFuturo);
+
+            //EXEC 7
+            // double Real, cotacaoDolar, Dolar;
+            // Console.Write("Escreva valor em R$: ");
+            // Real = double.Parse(Console.ReadLine());
+            // Console.Write("Escreva quanto está a cotação do $: ");
+            // cotacaoDolar = double.Parse(Console.ReadLine());
+            //Dolar = Real / cotacaoDolar;
+
+            // Console.WriteLine("O valor em dólares é: " + Dolar);
+
+            //EXEC8
+            //int triplo, dobro, soma, num;
+            //Console.Write("Escreva um número inteiro: ");
+            //num = int.Parse(Console.ReadLine());
+            //triplo = 3 * num;
+            //dobro = 2 * num;
+            //soma = (triplo + 1) + (dobro - 1);
+
+            //Console.WriteLine("O resultado desse valor é: " + soma);
+
+            //EXEC9
+
+            //int num;
+
+            //Console.Write("Escreva um número que seja inteiro e tenha 4 dígitos: ");
+            //num = int.Parse(Console.ReadLine());
+            //Console.WriteLine(num / 1000);
+            //Console.WriteLine((num / 100) % 10);
+            //Console.WriteLine((num / 10) % 10);
+            //Console.WriteLine(num % 10);
+
+            //EXEC10
+            //Dia 1:ocorreu uma queda de 3,4%
+            //investimento = investimento * (1 - 0.034);
+            //Dia 2:obteve uma subida de 2,8%
+            //investimento = investimento * (1 + 0.028);
+            //Dia 3: obteve mais uma subida de 14%
+            //investimento = investimento * (1 + 0.14);
+            //Dia 4: ocorreu mais uma queda, mas agora é de 8,5%
+            //investimento = investimento * (1 - 0.085);
+
+            //Console.WriteLine("O valor atual dos investimentos é de R${0:0.00}", investimento);
+
+            //EXEC11
+            //Console.Write("Escreva um número: ");
+            //int n = int.Parse(Console.ReadLine());
+            //int quadrado = n * n;
+            //int cubo = n * n * n;
+
+            //Console.WriteLine("O número {0} ao quadrado é {1}", n, quadrado);
+            //Console.WriteLine("O número {0} ao cubo é {1}", n, cubo); 
+
+            //EXEC12
+            //Letra A)
+            //Console.Write("Escreva a altura em metros: ");
+            //double altura = double.Parse(Console.ReadLine());
+
+            //double pesoIdeal = (72.7 * altura) - 58;
+
+            //Console.WriteLine("Com base nessas características, esse resultado deve ser o seu peso ideal {0}m é {1}kg", altura, pesoIdeal);
+
+            //Letra B)
+            //double pesoIdeal//Console.Write("Escreva a altura em metros: ");
+            //double altura = double.Parse(Console.ReadLine());
+
+            //double pesoIdeal = (62.1*h) -44.7
+            //Console.WriteLine("Com base nessas características, esse resultado deve ser o seu peso ideal {0}m é {1}kg", altura, pesoIdeal);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1_PR2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //exec 1
+            int metro,cm,mm;
+            Console.WriteLine("Digite o Número de metros");
+            metro=int.Parse(Console.ReadLine());
+            cm=metro*100;
+            mm=metro* 1000;
+            Console.WriteLine("A quantidade em cm é "+cm);
+            Console.WriteLine("A quantidade em mm é "+mm);
+            Console.ReadKey();
+        }
+    }
+}
